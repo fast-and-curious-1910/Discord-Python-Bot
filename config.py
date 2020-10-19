4aa90201bc45bb0eaa1e81dd0f357fabde4eb661
Palash Sureka <64191803+fast-and-curious-1910@users.noreply.github.com>
2020-10-19 20:00:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# To avoid Reddit's rate limit. In Seconds
+wait_time = 3
+
+# Your Discord webhook settings.
+webhook_url = "YOUR WEBHOOK URL"
+embed_title = "Title"
+
+# Max post limit, 10 is recommended
+limit = 48
+
+# Add subreddits to track. You can use as much as subreddit you want.
+
+sub1 = input('Subreddit One: ')
+sub2 = input('Subreddit 2 (Opt): ')
+
+subreddits = [sub1, sub2]
+
+# Reddit information . This is Example. THIS IS NOT REAL INFORMATION
+client_id="SI8pN3DSbt0zor"
+client_secret="xaxkj7HNh8kwg8e5t4m6KvSrbTI"
+password="1guiwevlfo00esyy"
+user_agent="testscript by u/fakebot3"
+username="fakebot3"
+
+
+# See README.md for more info
